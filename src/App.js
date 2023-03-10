ec988899819e8f07c4cf417dde8d8c5f6acca2d9
alszero <alszero05@gmail.com>
2023-03-10 11:48:26 +0900

    OR연산자 연습

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   const name = undefined;
-  return name || '값이 undefined 입니다.'; // 값이 거짓이어도 뒤에를 체크해야 된다.
+  return <div>{name || '리액트'}</div>
 }
 
 export default App;
