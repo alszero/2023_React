5ac958f6b29556f0407df504340151c5195dc4a7
alszero <alszero05@gmail.com>
2023-03-10 12:05:38 +0900

    css로 style 주기

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import './App.css';
 
 function App() {
   const name = '리액트';
-  return (
-    <div
-      style={{
-        backgroundColor : 'yellow', // background-Color ---> bacgroundColor로 표현(카멜)
-        color : 'aqua',
-        fontSize : '48px',
-        fontWeight : 'bold',
-        padding : 16 //단위가 px
-        
-      }}
-    >
-      name
-    </div>
-  );
+  return <div className="react">{name}</div>
 }
 
 export default App;
