d7abc22568e71162e2205078d8b9e091c21147fd
alszero <alszero05@gmail.com>
2023-03-10 11:58:48 +0900

    style 넣어주기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 
 function App() {
-  const name = undefined;
-  return <div>{name || '리액트'}</div>
+  const name = '리액트';
+  return (
+    <div
+      style={{
+        backgroundColor : 'yellow', // background-Color ---> bacgroundColor로 표현(카멜)
+        color : 'aqua',
+        fontSize : '48px',
+        fontWeight : 'bold',
+        padding : 16 //단위가 px
+        
+      }}
+    >
+      name
+    </div>
+  );
 }
 
 export default App;
