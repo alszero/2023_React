22b4c7755739466988c8c5fe40e05988204484c9
alszero <alszero05@gmail.com>
2023-03-10 12:17:01 +0900

    주석 작성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 
-function App() {
+function App(){
   const name = '리액트';
-  return <div className="react">{name}</div>
+  return (
+    <>
+    {/* 주석은 이렇게 작성합니다 */}
+    <div
+      className="react" //시작 태그를 여러줄로 작성하게 된다면 주석이 된다.
+      // 주석 되나요
+      >{name}
+      </div>
+      // 하지만 이런 주석이나
+      /* 이런 주석은 화면에 그대로 나타나게 된다.*/
+      <input/>
+        </>
+      )
 }
 
 export default App;
