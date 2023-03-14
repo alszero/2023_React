54c05537162890b45cb52a363b128b08a13c68c3
alszero <alszero05@gmail.com>
2023-03-14 14:40:13 +0900

    클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import './App.css';
-
-function App(){
-  const name = '리액트';
-  return (
-    <>
-    {/* 주석은 이렇게 작성합니다 */}
-    <div
-      className="react" //시작 태그를 여러줄로 작성하게 된다면 주석이 된다.
-      // 주석 되나요
-      >{name}
-      </div>
-      // 하지만 이런 주석이나
-      /* 이런 주석은 화면에 그대로 나타나게 된다.*/
-      <input/>
-        </>
-      )
+import "./App.css";
+import { Component } from "react";
+class App extends Component {
+render() {
+const name = "react";
+return <div className="react">{name}</div>;
 }
-
+}
 export default App;
