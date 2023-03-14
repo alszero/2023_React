9423d381ba09c5b3ca7ebf29333fde8aaa6a6ba4
alszero <alszero05@gmail.com>
2023-03-14 14:56:03 +0900

    기본이름입니다 추가

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+//MyComponent.js
+//function ---> =() => {}
+const MyComponent = props => {
+    return <div>안녕하세요, 제 이름은 {props.name} 입니다.</div>
+};
+
+MyComponent.defaultProps = {
+    name: "기본이름"
+};
+
+export default MyComponent;
