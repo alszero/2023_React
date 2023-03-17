d48d8bb0e9a776e91a770ee819aeb8de927d4d49
alszero <alszero05@gmail.com>
2023-03-17 11:38:00 +0900

    클래스 방식으로 표현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import MyComponent from "./MyComponent";
-const App = () => {
-return <MyComponent>리액트</MyComponent>
-};
+import MyComponent from "./MyComponent"; // 모듈 불러오기
+
+const  App = () =>  {
+    return <MyComponent name={"react"} favoriteNumber={2}>리액트</MyComponent> // 하나의 자식 속성 값으로 들어감
+  };
+
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,17 +1,28 @@
 //MyComponent.js
 //function ---> =() => {}
-const MyComponent = ({ name, children }) => {
-    return (
-    <div>
-    안녕하세요, 제 이름은 {name} 입니다. <br />
-    children 값은 {children}
-    입니다.
-    </div>
-    );
+import PropTypes from "prop-types"
+import { Component } from "react";
+class MyComponent extends Component {
+    render() {
+        const {name, favoriteNumber, children} = this.props;
+        // this.props를 통해 조회함
+        return (
+            <div>
+            안녕하세요, 제 이름은 {name} 입니다. <br />
+            children 값은 {children}
+            입니다. <br />
+            제가 좋아하는 숫자는 {favoriteNumber} 입니다.
+            </div>
+            );
+    }
+    
 };
 
 MyComponent.defaultProps = {
-    name: "기본이름"
+    name: "기본이름",
 };
-
+MyComponent.propTypes = {
+    name:PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+}
 export default MyComponent;
