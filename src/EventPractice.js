c39de364108e5149bfd336620c077c3878dff327
alszero <alszero05@gmail.com>
2023-03-28 15:11:30 +0900

    onClick으로 변환

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,25 +1,20 @@
-import { Component } from "react";
+import { useState } from "react";
 
-class EventPractice extends Component {
-  state = {
-    username: "",
-    message: "",
-  };
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    // 생성자 메소드는 this(현재객체)와 메소드를 묶어주는 역할을 한다.
-  }
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-      // 첫번째 input박스를 선택하면 [e.target.name]값은 username이 된다.
-      // 두번째 input박스를 선택하면 [e.target.name]값은 message가 된다.
-    });
+const EventPractice = () => {
+  const [username, setUsername] = useState("");
+  // username=''
+  // setUsername는 username값을 변경할 때 사용
+  const [message, setMessage] = useState("");
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value); //username값을 변경
   };
 
-  handleClick = () => {
+  const onChangeMessage = (e) => {
+    setMessage(e.target.value); //username값을 변경
+  };
+
+  const onClick = () => {
     alert(this.state.username + ":" + this.state.message);
     this.setState({
       username: "",
@@ -27,33 +22,32 @@ class EventPractice extends Component {
     });
   };
 
-  handleKeyDown = (e) => {
+  const onKeyDown = (e) => {
     if (e.key === "Enter") {
       this.handleClick();
     }
   };
-  render() {
-    return (
-      <div>
-        <h1>이벤트 연습</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="사용자 이름"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="message"
-          placeholder="아무거나 입력해 보세요"
-          value={this.state.message}
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-        />
-        <button onClick={this.handleClick}>확인</button>
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="사용자 이름"
+        value={username}
+        onChange={onChangeUsername}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해 보세요"
+        value={message}
+        onChange={onChangeMessage}
+        onKeyDown={onKeyDown}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 export default EventPractice;
