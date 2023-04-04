650718785f1432dc393ea4dc2a710d777aff21a7
alszero <alszero05@gmail.com>
2023-04-04 15:47:16 +0900

    배열로 변경

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,9 +1,8 @@
 const IterationSample = () => {
   const names = ["눈사람", "얼음", "눈", "바람"];
-  // names 배열변수에 데이터를 저장
-  const nameList = names.map((name) => <li>{name}</li>);
-  // names 배열에 있는 데이터를 1개씩 순차적으로 출려하는
-  // 데이터를 nameList 변수에 저장
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  // 배열의 index값을 key값으로 설정할 때 배열이 변경되면 렌더링을 하지 못하는 경우가 발생
+  // 배열의 index값을 key값으로 하는 것은 바람직하지 않다.
   return <ul>{nameList}</ul>;
 };
 export default IterationSample;
