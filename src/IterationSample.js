852c21821833f5914deab32766eaf909e3b65013
alszero <alszero05@gmail.com>
2023-04-07 10:48:37 +0900

    리스트 삭제 추가 기능 넣기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,8 +1,42 @@
+// IterationSample.js
+import { useState } from "react";
 const IterationSample = () => {
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
-  // 배열의 index값을 key값으로 설정할 때 배열이 변경되면 렌더링을 하지 못하는 경우가 발생
-  // 배열의 index값을 key값으로 하는 것은 바람직하지 않다.
-  return <ul>{nameList}</ul>;
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  // 새로운 항목을 추가할때 사용할 id
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId에 1을 더해준다.
+    setNames(nextNames); // names 값을 업데이트 한다.
+    setInputText(""); // inputText를 비운다.
+  };
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 export default IterationSample;
